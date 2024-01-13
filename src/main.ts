0773eff3facea3d3694c0a62af55830509170435
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-13 23:50:34 +0000

    adds global prefix

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
