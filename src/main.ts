fd09ef26c1c1017902c2dec8fd09cdf76ea9342c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-13 21:09:17 +0000

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
