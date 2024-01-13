d112134b40e4ff71af0ce8ed9223fa43744bb9b5
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-13 23:55:46 +0000

    adds limit rate with throttlers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 3 }]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
