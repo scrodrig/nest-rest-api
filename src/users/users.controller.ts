08a494f7edb31a8f76b2da33067fdfae62d9596c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-13 20:09:26 +0000

    adds GET, POST, PATCH, DELETE methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() //All /users or /users/?role=John
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get('/interns')
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() user: {}) {
+    return { id, ...user };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
