318e46c1581553f710e971cbb50a78a08983d9a4
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-14 00:31:15 +0000

    adds logs globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { AppModule } from './app.module';
+import { MyLoggerService } from './my-logger/my-logger.service';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {bufferLogs: true});
+  app.useLogger(app.get(MyLoggerService));
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3000);
